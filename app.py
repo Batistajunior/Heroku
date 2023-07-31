8b0479dadbfb47768eca93994e8762d3fcc0a798
Antonio Carlos Batista Junior <batistajunior@MacBook-Air-de-Antonio.local>
2023-07-31 10:44:37 -0300

    Adicionar código do aplicativo Flask

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Flask Heroku application"
+def index():
+    return 'Bem-vindo ao meu aplicativo Flask no Heroku!'
 
 if __name__ == '__main__':
     app.run()
