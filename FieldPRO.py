939b462ee4f1c36aa81943388d052f3d890de0b1
Batista Junior <junior.engenheirobatista@gmail.com>
2023-07-31 20:56:11 -0300

    Update FieldPRO.py

--- FieldPRO.py
+++ FieldPRO.py
@@ -35,10 +35,6 @@ def prepare_data():
         # Juntando os DataFrames df_sensor e df_estacao com base nas colunas 'data' e 'Hora (Brasília)'
         df_completo = pd.merge(df_sensor, df_estacao, on=['data', 'Hora (Brasília)'], how='inner')
 
-    # Etapa 4: Análise exploratória dos dados e visualização de gráficos
-    # Verificando quais colunas têm dados válidos para plotar os gráficos
-    valid_columns = ['air_humidity_100', 'air_temperature_100', 'atm_pressure_main', 'num_of_resets', 'piezo_charge', 'piezo_temperature', 'chuva']
-
     # Resto do código para a análise exploratória dos dados e visualização de gráficos...
 
     return df_completo
@@ -105,7 +101,6 @@ def treinamento():
     # Exibindo as métricas do modelo e os dados de treinamento
     return f"Erro Médio Absoluto (MAE): {mae}<br>Erro Quadrático Médio (MSE): {mse}<br>Coeficiente de Determinação (R²): {r2}"
 
-
 @app.route('/inicio')
 def index():
     return 'Bem-vindo ao meu aplicativo Flask no Heroku!'
@@ -116,102 +111,11 @@ def visualizar_graficos():
     df_sensor = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/Heroku/main/Sensor_FieldPRO.csv')
     df_estacao = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/Heroku/main/Estacao_Convencional.csv')
 
-    
+    # Restante do código para visualização de gráficos...
 
-    # Etapa 3: Pré-processamento dos dados
-    df_sensor['Datetime – utc'] = pd.to_datetime(df_sensor['Datetime – utc'], format='ISO8601', utc=True)
-    df_sensor['data'] = df_sensor['Datetime – utc'].dt.date
-    df_sensor['Hora (Brasília)'] = df_sensor['Datetime – utc'].dt.time
-    df_sensor['data'] = pd.to_datetime(df_sensor['data'])
-    df_sensor['Hora (Brasília)'] = pd.to_datetime(df_sensor['Hora (Brasília)'], format='%H:%M:%S', errors='coerce').dt.time
-    df_sensor = df_sensor.drop(columns=['Datetime – utc'])
-
-    # Reordenando as colunas com 'data' e 'Hora (Brasília)' na frente
-    df_sensor = df_sensor[['data', 'Hora (Brasília)', 'air_humidity_100', 'air_temperature_100', 'atm_pressure_main', 'num_of_resets', 'piezo_charge', 'piezo_temperature']]
-
-    # Convertendo as colunas 'data' e 'Hora (Brasília)' para o tipo datetime no DataFrame df_estacao
-    df_estacao['data'] = pd.to_datetime(df_estacao['data'])
-    df_estacao['Hora (Brasília)'] = pd.to_datetime(df_estacao['Hora (Brasília)'], format='%H:%M:%S', errors='coerce').dt.time
-
-    # Juntando os DataFrames df_sensor e df_estacao com base nas colunas 'data' e 'Hora (Brasília)'
-    df_completo = pd.merge(df_sensor, df_estacao, on=['data', 'Hora (Brasília)'], how='inner')
-
-    # Etapa 4: Análise exploratória dos dados e visualização de gráficos
-    # Verificando quais colunas têm dados válidos para plotar os gráficos
-    valid_columns = ['air_humidity_100', 'air_temperature_100', 'atm_pressure_main', 'num_of_resets', 'piezo_charge', 'piezo_temperature', 'chuva']
-
-    # Verificando se há alguma coluna com dados válidos para plotar os gráficos
-    if all(df_completo[col].notnull().any() for col in valid_columns):
-        # Visualizando a distribuição das variáveis e plotando os gráficos
-        fig, axes = plt.subplots(1, 7, figsize=(24, 5))
-        for i, col in enumerate(valid_columns):
-            sns.histplot(df_completo[col], bins=20, edgecolor='black', color='skyblue', alpha=0.8, ax=axes[i])
-            axes[i].set_title(col)
-        plt.tight_layout()
-        plt.show()
-
-        # Verificando a correlação entre as variáveis numéricas
-        correlation_matrix = df_completo[valid_columns].corr()
-
-        # Plotando a matriz de correlação
-        plt.figure(figsize=(10, 8))
-        sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=1, linecolor='black', annot_kws={"size": 10})
-        plt.xticks(rotation=45, ha='right')
-        plt.yticks(rotation=0)
-        plt.title('Matriz de Correlação', fontsize=14)
-        plt.show()
-
-    # Etapa 5: Preparação dos dados para treinamento do modelo
-    # Definindo as features e o target
-    features = ['air_humidity_100', 'air_temperature_100', 'atm_pressure_main', 'num_of_resets', 'piezo_charge', 'piezo_temperature']
-    target = 'chuva'
-
-    # Removendo amostras com valores ausentes
-    df_completo.dropna(subset=features + [target], inplace=True)
-
-    # Separando os dados em features (X) e target (y)
-    X = df_completo[features]
-    y = df_completo[target]
-
-    # Dividindo os dados em conjuntos de treino e teste
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # Preenchendo valores ausentes com a média
-    imputer = SimpleImputer(strategy='mean')
-    X_train_filled = imputer.fit_transform(X_train)
-    X_test_filled = imputer.transform(X_test)
-
-    # Etapa 6: Treinamento e avaliação do modelo
-    # Criando e treinando o modelo de regressão com HistGradientBoostingRegressor
-    model = HistGradientBoostingRegressor()
-    model.fit(X_train_filled, y_train)
-
-    # Realizando as previsões no conjunto de teste
-    y_pred = model.predict(X_test_filled)
-
-    # Avaliando o desempenho do modelo
-    mae = mean_absolute_error(y_test, y_pred)
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-
-    print("Erro Médio Absoluto (MAE):", mae)
-    print("Erro Quadrático Médio (MSE):", mse)
-    print("Coeficiente de Determinação (R²):", r2)
-
-    # Etapa 7: Salvando o modelo treinado
-    # Salvando o modelo em um arquivo joblib
-    modelo_treinado_path = "modelo_treinado.joblib"
-    joblib.dump(model, modelo_treinado_path)
-
-    return f"Erro Médio Absoluto (MAE): {mae}<br>Erro Quadrático Médio (MSE): {mse}<br>Coeficiente de Determinação (R²): {r2}"
-
+    return "Gráficos visualizados com sucesso!"
 
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
- 
-
-
-
