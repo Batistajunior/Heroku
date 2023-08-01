59487ec7cc57f7acb5d6c7fe594db3fcc3184df0
Batista Junior <junior.engenheirobatista@gmail.com>
2023-07-31 22:33:32 -0300

    Update FieldPRO.py

--- FieldPRO.py
+++ FieldPRO.py
@@ -118,7 +118,53 @@ def visualizar_graficos():
     df_sensor = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/Heroku/main/Sensor_FieldPRO.csv')
     df_estacao = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/Heroku/main/Estacao_Convencional.csv')
 
-    # Restante da sua lógica para visualização de gráficos...
+    @app.route('/visualizar-graficos')
+    def visualizar_graficos():
+    # Etapa 1: Carregamento dos dados
+     df_sensor = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/desafio-de-dados-fieldpro/main/Sensor_FieldPRO.csv')
+    df_estacao = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/desafio-de-dados-fieldpro/main/Estacao_Convencional.csv')
+
+    # Juntando os DataFrames df_sensor e df_estacao com base nas colunas 'data' e 'Hora (Brasília)'
+    df_completo = pd.merge(df_sensor, df_estacao, on=['data', 'Hora (Brasília)'], how='inner')
+
+    # Etapa 2: Visualização de gráficos
+    # Exemplo de criação de gráfico de chuva ao longo do tempo
+    plt.figure(figsize=(10, 6))
+    plt.plot(df_completo['data'], df_completo['chuva'], label='Chuva')
+    plt.xlabel('Data')
+    plt.ylabel('Chuva')
+    plt.title('Gráfico de Chuva ao longo do tempo')
+    plt.legend()
+
+    # Salvar o gráfico em um buffer de bytes para inserir no template HTML
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plot_data = base64.b64encode(buffer.read()).decode()
+    plt.close()
+
+    # Etapa 3: Matriz de correlação
+    # Selecionando as colunas relevantes para a matriz de correlação
+    valid_columns = ['air_humidity_100', 'air_temperature_100', 'atm_pressure_main', 'num_of_resets', 'piezo_charge', 'piezo_temperature', 'chuva']
+    correlation_matrix = df_completo[valid_columns].corr()
+
+    # Plotando a matriz de correlação
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=1, linecolor='black', annot_kws={"size": 10})
+    plt.xticks(rotation=45, ha='right')
+    plt.yticks(rotation=0)
+    plt.title('Matriz de Correlação', fontsize=14)
+
+    # Salvar a matriz de correlação em um buffer de bytes para inserir no template HTML
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    correlation_plot_data = base64.b64encode(buffer.read()).decode()
+    plt.close()
+
+    # Renderizar o template HTML com os gráficos
+    return render_template('visualizar_graficos.html', plot_data=plot_data, correlation_plot_data=correlation_plot_data)
+
 
     return "Gráficos visualizados com sucesso!"
 
