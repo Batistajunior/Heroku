25b6a20c2dda988a01af20d2b532bc1a96676d68
Batista Junior <junior.engenheirobatista@gmail.com>
2023-07-31 21:56:39 -0300

    Update FieldPRO.py

--- FieldPRO.py
+++ FieldPRO.py
@@ -11,10 +11,13 @@ app = Flask(__name__)
 df_sensor = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/desafio-de-dados-fieldpro/main/Sensor_FieldPRO.csv')
 df_estacao = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/desafio-de-dados-fieldpro/main/Estacao_Convencional.csv')
 
-def create_plot():
+# Junção dos DataFrames df_sensor e df_estacao para criar df_completo
+df_completo = pd.merge(df_sensor, df_estacao, on=['data', 'Hora (Brasília)'], how='inner')
+
+def create_analise_plot():
     # Exemplo de criação de gráfico usando Matplotlib e Seaborn
     plt.figure(figsize=(10, 6))
-    plt.plot(df_sensor['data'], df_sensor['chuva'], label='Chuva')
+    plt.plot(df_completo['data'], df_completo['chuva'], label='Chuva')
     plt.xlabel('Data')
     plt.ylabel('Chuva')
     plt.title('Gráfico de Chuva ao longo do tempo')
@@ -35,32 +38,16 @@ def index_inicio():
 
 @app.route('/analise')
 def analise():
-    # Etapa 1: Carregamento dos dados
-    df_sensor = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/desafio-de-dados-fieldpro/main/Sensor_FieldPRO.csv')
-    df_estacao = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/desafio-de-dados-fieldpro/main/Estacao_Convencional.csv')
-
     # Exemplo de criação de gráfico usando Matplotlib e Seaborn
-    plt.figure(figsize=(10, 6))
-    plt.plot(df_sensor['data'], df_sensor['chuva'], label='Chuva')
-    plt.xlabel('Data')
-    plt.ylabel('Chuva')
-    plt.title('Gráfico de Chuva ao longo do tempo')
-    plt.legend()
+    plot_data = create_analise_plot()
 
-    # Salvar o gráfico em um buffer de bytes para inserir no template HTML
-    buffer = io.BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-    plot_data = base64.b64encode(buffer.read()).decode()
-    plt.close()
-
-    # Renderizar o template HTML com o gráfico
+    # Renderizar o template HTML com o gráfico de análise
     return render_template('analise.html', plot_data=plot_data)
 
 @app.route('/visualizar-graficos')
 def visualizar_graficos():
     # Criação dos gráficos
-    plot_data_1 = create_plot()
+    plot_data_1 = create_analise_plot()
 
     # Renderizar o template HTML com os gráficos
     return render_template('visualizar_graficos.html', plot_data_1=plot_data_1)
