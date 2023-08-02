fdd4d6fcb545930de6b442a621704e894381f90e
Batista Junior <junior.engenheirobatista@gmail.com>
2023-08-01 21:22:30 -0300

    Update FieldPRO.py

--- FieldPRO.py
+++ FieldPRO.py
@@ -1,80 +1,98 @@
-from flask import Flask, render_template
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import base64
-import io
+import plotly.express as px
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from sklearn.model_selection import train_test_split
+from sklearn.experimental import enable_hist_gradient_boosting
+from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import joblib
 
-app = Flask(__name__)
+import warnings
+warnings.filterwarnings("ignore")
 
-# DataFrame para armazenar os dados
-df_completo = None
+# Etapa 1: Carregamento dos dados
+df_sensor = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/Heroku/main/df_sensor.csv')
+df_estacao = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/Heroku/main/Estacao_Convencional.csv')
 
-@app.route('/')
-def index_inicio():
-    return 'Bem-vindo ao meu aplicativo Flask no Heroku!'
+# Etapa 2: Juntando os DataFrames df_sensor e df_estacao com base nas colunas 'data' e 'Hora (Brasília)'
+df_completo = pd.merge(df_sensor, df_estacao, on=['data', 'Hora (Brasília)'], how='inner')
 
-def prepare_data():
-    global df_completo
+# Etapa 4: Análise exploratória dos dados e visualização de gráficos
+# Verificando quais colunas têm dados válidos para plotar os gráficos
+valid_columns = ['air_humidity_100', 'air_temperature_100', 'atm_pressure_main', 'num_of_resets', 'piezo_charge', 'piezo_temperature', 'chuva']
 
-    # Carrega os DataFrames df_sensor e df_estacao, se ainda não foram carregados
-    if df_completo is None:
-        df_sensor = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/Heroku/main/df_sensor.csv')
-        df_estacao = pd.read_csv('https://raw.githubusercontent.com/Batistajunior/Heroku/main/Estacao_Convencional.csv')
+# Verificando se há alguma coluna com dados válidos para plotar os gráficos
+if all(df_completo[col].notnull().any() for col in valid_columns):
+    # Visualizando a distribuição das variáveis e plotando os gráficos
+    fig_hist = plt.figure(figsize=(24, 5))
+    for i, col in enumerate(valid_columns):
+        plt.subplot(1, 7, i+1)
+        sns.histplot(df_completo[col], bins=20, edgecolor='black', color='skyblue', alpha=0.8)
+        plt.title(col)
+    plt.tight_layout()
 
-        # Juntando os DataFrames df_sensor e df_estacao com base nas colunas 'data' e 'Hora (Brasília)'
-        df_completo = pd.merge(df_sensor, df_estacao, on=['data', 'Hora (Brasília)'], how='inner')
-
-    return df_completo
-
-@app.route('/visualizar_graficos')
-def visualizar_graficos():
-    global df_completo
-
-    # Verificar se o DataFrame df_completo está carregado
-    if df_completo is None:
-        # Se não estiver carregado, chamar a função prepare_data() para carregar os dados
-        df_completo = prepare_data()
-
-    # Etapa 2: Visualização de gráficos
-    # Exemplo de criação de gráfico de chuva ao longo do tempo
-    plt.figure(figsize=(10, 6))
-    plt.plot(df_completo['data'], df_completo['chuva'], label='Chuva')
-    plt.xlabel('Data')
-    plt.ylabel('Chuva')
-    plt.title('Gráfico de Chuva ao longo do tempo')
-    plt.legend()
-
-    # Salvar o gráfico em um buffer de bytes para inserir no template HTML
-    buffer = io.BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-    plot_data = base64.b64encode(buffer.read()).decode()
-    plt.close()
-
-    # Etapa 3: Matriz de correlação
-    # Selecionando as colunas relevantes para a matriz de correlação
-    valid_columns = ['air_humidity_100', 'air_temperature_100', 'atm_pressure_main', 'num_of_resets', 'piezo_charge', 'piezo_temperature', 'chuva']
+    # Verificando a correlação entre as variáveis numéricas
     correlation_matrix = df_completo[valid_columns].corr()
 
     # Plotando a matriz de correlação
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=1, linecolor='black', annot_kws={"size": 10})
-    plt.xticks(rotation=45, ha='right')
-    plt.yticks(rotation=0)
-    plt.title('Matriz de Correlação', fontsize=14)
+    fig_corr = px.imshow(correlation_matrix)
 
-    # Salvar a matriz de correlação em um buffer de bytes para inserir no template HTML
-    buffer = io.BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-    correlation_plot_data = base64.b64encode(buffer.read()).decode()
-    plt.close()
+# Etapa 5: Preparação dos dados para treinamento do modelo
+# Definindo as features e o target
+features = ['air_humidity_100', 'air_temperature_100', 'atm_pressure_main', 'num_of_resets', 'piezo_charge', 'piezo_temperature']
+target = 'chuva'
 
-    # Renderizar o template HTML com os gráficos
-    return render_template('visualizar_graficos.html', plot_data=plot_data, correlation_plot_data=correlation_plot_data)
+# Removendo amostras com valores ausentes
+df_completo.dropna(subset=features + [target], inplace=True)
+
+# Separando os dados em features (X) e target (y)
+X = df_completo[features]
+y = df_completo[target]
+
+# Dividindo os dados em conjuntos de treino e teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Preenchendo valores ausentes com a média
+imputer = SimpleImputer(strategy='mean')
+X_train_filled = imputer.fit_transform(X_train)
+X_test_filled = imputer.transform(X_test)
+
+# Etapa 6: Treinamento e avaliação do modelo
+# Criando e treinando o modelo de regressão com HistGradientBoostingRegressor
+model = HistGradientBoostingRegressor()
+model.fit(X_train_filled, y_train)
+
+# Realizando as previsões no conjunto de teste
+y_pred = model.predict(X_test_filled)
+
+# Avaliando o desempenho do modelo
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("Erro Médio Absoluto (MAE):", mae)
+print("Erro Quadrático Médio (MSE):", mse)
+print("Coeficiente de Determinação (R²):", r2)
+
+# Salvando o modelo em um arquivo joblib
+modelo_treinado_path = "modelo_treinado.joblib"
+joblib.dump(model, modelo_treinado_path)
+
+# Criação do aplicativo Dash
+app = dash.Dash(__name__)
+
+# Layout do aplicativo
+app.layout = html.Div(children=[
+    html.H1('Análise Exploratória de Dados'),
+    dcc.Graph(id='histogram-plots', figure=fig_hist),
+    dcc.Graph(id='correlation-matrix-plot', figure=fig_corr)
+])
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run_server(debug=True)
